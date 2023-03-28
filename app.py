4663943a4b53a42beb3f504a9bd7e94598bea931
Ananya1301 <110127399+Ananya1301@users.noreply.github.com>
2023-03-29 00:50:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import plotly.express as px
 import streamlit_option_menu as option_menu
 
 with open("styles.css") as source_style:
-  st.markdown(f"<style>{source_style.read()}</style>",
-              unsafe_allow_html=True)
+    st.markdown(f"<style>{source_style.read()}</style>",
+                unsafe_allow_html=True)
 
 
 with st.sidebar:
@@ -38,81 +38,83 @@ if selected == "Home":
 
   st.markdown('This study aims to develop a Machine Learning system for monitoring the water quality of multiple lakes in the Bhopal region using satellite imagery and GIS techniques. The objective is to assess the water quality, detect the probable causes of water pollution and predict the potential impacts of different pollution sources on the environment and human health. The study will use a combination of remote sensing techniques, image processing methods, and GIS tools to extract relevant information from satellite data. Additionally, the system will enable real-time monitoring of water quality, allowing for timely intervention in case of any deterioration in water quality.')
 
-
+ 
 elif selected == "EDA":
-  st.markdown("""<h style = " "> Exploratory Data Analysis </h>""", unsafe_allow_html=True)
-
+  st.markdown("""<h style = " "> Exploratory Data Analysis </h>""", unsafe_allow_html = True)
+ 
   st.sidebar.subheader("Visualisation Settings")
 
-  chart_select = st.sidebar.selectbox(label="Select the Lake", options=['Hathaikheda Dam', 'Sarangpani Lake', 'Upper Lake', 'Lendiya Lake'])
 
+  chart_select = st.sidebar.selectbox(
+    label = "Select the Lake",
+    options = ['Hathaikheda dam', 'Sarangpani lake', 'Upper lake']
+  )
   if chart_select == 'Hathaikheda dam':
-    df = pd.read_csv('finalHK.csv')
-    st.subheader('Hathaikheda dam')
+      df = pd.read_csv('finalHK.csv')
+      st.subheader('Hathaikheda dam')
+  elif chart_select == 'Sarangpani lake' :
+     df = pd.read_csv('final-new.csv')
+     st.subheader('Sarangpani lake')
+  elif chart_select == 'Upper lake' :
+     df = pd.read_csv('finalUPL.csv')
+     st.subheader('Upper Lake')
 
-  elif chart_select == 'Sarangpani lake':
-    df = pd.read_csv('final-new.csv')
-    st.subheader('Sarangpani lake')
 
-  elif chart_select == 'Upper lake':
-    df = pd.read_csv('finalUPL.csv')
-    st.subheader('Upper Lake')
-
-  elif chart_select == 'Lendiya Lake':
-    df = pd.read_csv('Lendiya.csv')
-    st.subheader('Lendiya Lake')
-    
+   
   show_data = st.sidebar.checkbox("Show dataset")
 
   if show_data:
-        st.write(df)
+    st.write(df)
 
   global numeric_columns
   try:
-      numeric_columns = list(df.select_dtypes(['float', 'int']).columns)
+    numeric_columns  = list(df.select_dtypes(['float','int' ]).columns)
   except Exception as e:
-      print(e)
+    print(e)
+    
+
 
   chart_select = st.sidebar.selectbox(
-    label="Select the Chart Type",
-    options=['Scatterplots', 'Lineplots', 'Histogram', 'Boxplot'])
+    label = "Select the Chart Type",
+    options = ['Scatterplots', 'Lineplots', 'Histogram', 'Boxplot']
+  )
 
   if chart_select == 'Scatterplots':
     st.sidebar.subheader('Scatterplot Settings')
     try:
-      x_values = st.sidebar.selectbox('X axis', options=numeric_columns)
-      y_values = st.sidebar.selectbox('Y axis', options=numeric_columns)
-      plot = px.scatter(data_frame=df, x=x_values, y=y_values)
-      st.plotly_chart(plot)
+        x_values = st.sidebar.selectbox('X axis', options = numeric_columns)
+        y_values = st.sidebar.selectbox('Y axis', options = numeric_columns)
+        plot = px.scatter(data_frame = df, x = x_values, y = y_values)
+        st.plotly_chart(plot)
     except Exception as e:
-      print(e)
+        print(e)
 
   if chart_select == 'Lineplots':
     st.sidebar.subheader('Lineplots Settings')
     try:
-      x_values = st.sidebar.selectbox('X axis', options=numeric_columns)
-      y_values = st.sidebar.selectbox('Y axis', options=numeric_columns)
-      plot = px.area(data_frame=df, x=x_values, y=y_values)
-      st.plotly_chart(plot)
+        x_values = st.sidebar.selectbox('X axis', options = numeric_columns)
+        y_values = st.sidebar.selectbox('Y axis', options = numeric_columns)
+        plot = px.area(data_frame = df, x = x_values, y = y_values)
+        st.plotly_chart(plot)
     except Exception as e:
-      print(e)
+        print(e)
 
   if chart_select == 'Boxplot':
     st.sidebar.subheader('Boxplot Settings')
     try:
-      x_values = st.sidebar.selectbox('X axis', options=numeric_columns)
-      y_values = st.sidebar.selectbox('Y axis', options=numeric_columns)
-      plot = px.box(data_frame=df, x=x_values, y=y_values)
-      st.plotly_chart(plot)
+        x_values = st.sidebar.selectbox('X axis', options = numeric_columns)
+        y_values = st.sidebar.selectbox('Y axis', options = numeric_columns)
+        plot = px.box(data_frame = df, x = x_values, y = y_values)
+        st.plotly_chart(plot)
     except Exception as e:
-      print(e)
+        print(e)
 
   if chart_select == 'Histogram':
     st.sidebar.subheader('Histogram Settings')
     try:
-      x_values = st.sidebar.selectbox('Select the variable to plot histogram', options=numeric_columns)
-      bins = st.sidebar.slider("Select the number of bins", min_value=5, max_value=50, value=20, step=1)
-      plot = px.histogram(data_frame=df, x=x_values, nbins=bins)
-      st.plotly_chart(plot)
+        x_values = st.sidebar.selectbox('Select the variable to plot histogram', options = numeric_columns)
+        bins = st.sidebar.slider("Select the number of bins", min_value=5, max_value=50, value=20, step=1)
+        plot = px.histogram(data_frame = df, x = x_values, nbins=bins)
+        st.plotly_chart(plot)
     except Exception as e:
-      print(e)
+        print(e)
